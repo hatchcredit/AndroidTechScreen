d6a59bc2ca7b2ce4847a9d8679f892f09a2d7c8a
Parth Padgaonkar <parth@hatchcard.com>
2021-12-03 11:37:47 -0500

    helpers

--- app/src/main/java/nearside/interview/android/ui/main/MainViewModel.kt
+++ app/src/main/java/nearside/interview/android/ui/main/MainViewModel.kt
@@ -1,7 +1,28 @@
 package nearside.interview.android.ui.main
 
+import android.graphics.Bitmap
+import android.graphics.drawable.Drawable
+import android.util.Base64
+import androidx.core.graphics.drawable.toBitmap
 import androidx.lifecycle.ViewModel
+import java.io.ByteArrayOutputStream
+
+private const val IMAGE_URL = "https://picsum.photos/25?grayscale"
 
 class MainViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
+
+
 }
+
+private fun Drawable?.drawableToBytes(): ByteArray {
+    val bitmap = this?.toBitmap(25, 25) ?: return byteArrayOf()
+    val bytes = ByteArrayOutputStream(bitmap.allocationByteCount).use {
+        bitmap.compress(Bitmap.CompressFormat.JPEG, 75, it)
+        it.toByteArray()
+    }
+    bitmap.recycle()
+    return bytes
+}
+
+private fun ByteArray.bytesToHexString() = joinToString(separator = "") { "%02x".format(it) }
+private fun ByteArray.toBase64() = Base64.encodeToString(this, Base64.DEFAULT)
