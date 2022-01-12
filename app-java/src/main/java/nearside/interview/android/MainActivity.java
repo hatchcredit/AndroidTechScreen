308bc16369780f725376603c9d36b1c7fbb563dc
Parth Padgaonkar <parth@nearside.com>
2022-01-12 16:41:51 -0500

    fixes

--- app-java/src/main/java/nearside/interview/android/MainActivity.java
+++ app-java/src/main/java/nearside/interview/android/MainActivity.java
@@ -6,7 +6,7 @@ import android.os.PersistableBundle;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
-class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity {
 
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState, @Nullable PersistableBundle persistentState) {
---
+++ app-java/src/test/java/nearside/interview/android/ui/main/MainViewModelTest.java
@@ -0,0 +1,12 @@
+package nearside.interview.android.ui.main;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class MainViewModelTest {
+
+    @Test public void test(){
+
+    }
+}
